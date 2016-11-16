0f400f7fa4ba4d46740bf90b145e1c6c4a8520aa
david <spm1dh@gmail.com>
2016-11-15 23:04:58 -0500

    stos mens

---
+++ src/aplicacion/Ventana.java
@@ -0,0 +1,46 @@
+//package aplicacion;
+import java.util.*;
+/**
+*Clase Ventana 
+*@autor Andres Felipe Pardo Mesa
+*@autor David Felipe Vaca Santa 
+**/
+public class Ventana {
+	private boolean reparada;
+	private int numVidrios;
+	private int vidriosRepar;
+	private boolean tieneVecino;
+	private boolean tieneSorpresa;
+	//private Arraylist<Pared> paredes;
+	/**
+	 * Creador de la clase Ventana
+	 */
+	public void veantana(){
+		reparada=false;
+		numVidrios= 3;
+		vidriosRepar=1;
+		Random rand = new Random();
+		tieneVecino= rand.nextBoolean();	
+	}
+	/**
+	 * Nos permite saber la cantidad de vidrios por reparar.
+	 * @return int vidriosRepar 
+	 */
+	public int vidriosReparar(){
+		return vidriosRepar;
+	}
+	/**
+	 *Este metodo nos permite cambiar el estado de una ventana a reparada 
+	 */
+	public void reparar(){
+		vidriosRepar=0;
+		reparada=true;
+	}
+	/**
+	 * Este metodo nos permite saber si la ventana de algun piso tiene un vecino
+	 * @return boolean tieneVecino
+	 */
+	public boolean tieneVecino(){
+		return tieneVecino;
+	}
+}
