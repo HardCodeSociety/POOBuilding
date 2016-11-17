222f346a7f0b5fefa9c8ae2c50fc12094a97b6d5
Felipe Pardo <a.pardo6162@gmail.com>
2016-11-16 23:24:14 -0500

    Menenu principal sin oyentes

--- src/presentacion/POOBuildinGUI.java
+++ src/presentacion/POOBuildinGUI.java
@@ -11,15 +11,16 @@ public class POOBuildinGUI extends JFrame{
 	//private Partida partida;
 	
 	//Menu Principal
-	private JLabel logo;
+	private JPanel panelLogo;
 	private JPanel panelOpciones;
-	
+
+	//Elementos del panel logo
+	private JLabel logo;
 	//Elementos del panel de opciones
 	private JButton botonJugar;
 	private JButton botonAbrir;
 	private JButton botonInstrucciones;
 	private JButton botonImportar;
-	
 	
 	public POOBuildinGUI(){
 		//prepareAcciones();
@@ -27,24 +28,53 @@ public class POOBuildinGUI extends JFrame{
 		//partida = new Partida();
 		//inicie();
 	}
-	//elementosPanelBotones(){}
 	public void prepareElementos(){
-		setBackground(Color.BLACK);
-		setLayout(new BorderLayout(3,3));
-		logo = new JLabel();
-		logo.setBorder(BorderFactory.createEmptyBorder(10,320,10,320));
-		logo.setLayout(new GridLayout(1,4,7,7));
-		ImageIcon icono=new ImageIcon("imagenes/icono.jpg");
-		panelOpciones = new JPanel();
-		//elementosPanelBotones();
-		logo.setIcon(icono);
-		logo.setBackground(Color.BLACK);
-		add(logo,BorderLayout.NORTH);
-		add(panelOpciones,BorderLayout.CENTER);
 		Dimension screen=Toolkit.getDefaultToolkit().getScreenSize();
 		setSize(screen.width/2,screen.height/2);
+		setLayout(new BorderLayout(3,3));
+		panelLogo = new JPanel();
+		panelLogo.setLayout(new GridLayout(1,1,7,7));
+		panelLogo.setBackground(Color.BLACK);
+		panelLogo.setBorder(BorderFactory.createEmptyBorder(10,430,10,320));
+		elementosPanelLogo();
+		panelOpciones = new JPanel();
+		panelOpciones.setLayout(new GridLayout(4,1,7,7));
+		panelOpciones.setBackground(Color.BLACK);
+		panelOpciones.setBorder(BorderFactory.createEmptyBorder(10,320,10,320));
+		elementosPanelBotones();
+		add(panelLogo,BorderLayout.NORTH);
+		add(panelOpciones,BorderLayout.CENTER);
 	}
-	
+	public void elementosPanelBotones(){
+		Font fuente = new Font("Tahoma", Font.BOLD, 25);
+		botonJugar=new JButton("JUGAR");
+		botonJugar.setBackground(Color.BLACK); 
+        botonJugar.setFont(fuente); 
+		botonJugar.setForeground(Color.WHITE);
+		botonAbrir=new JButton("ABRIR");
+		botonAbrir.setBackground(Color.BLACK); 
+        botonAbrir.setFont(fuente); 
+		botonAbrir.setForeground(Color.WHITE);
+		botonInstrucciones=new JButton("INSTRUCCIONES");
+		botonInstrucciones.setBackground(Color.BLACK); 
+        botonInstrucciones.setFont(fuente); 
+		botonInstrucciones.setForeground(Color.WHITE);
+		botonImportar=new JButton("IMPORTAR");
+		botonImportar.setBackground(Color.BLACK); 
+        botonImportar.setFont(fuente); 
+		botonImportar.setForeground(Color.WHITE);
+		panelOpciones.add(botonJugar);
+		panelOpciones.add(botonAbrir);
+		panelOpciones.add(botonImportar);
+		panelOpciones.add(botonInstrucciones);
+	}	
+	public void elementosPanelLogo(){
+		logo=new JLabel();
+		ImageIcon icono=new ImageIcon("imagenes/icono.png");
+		logo.setBackground(Color.BLACK);
+		logo.setIcon(icono);
+		panelLogo.add(logo);
+	}
 	
 	
 	
