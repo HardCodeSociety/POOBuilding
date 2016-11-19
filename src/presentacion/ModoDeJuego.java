dc3a944ed9b6fa96e00dc5efa9a0676bb5091bdc
Felipe Pardo <a.pardo6162@gmail.com>
2016-11-18 21:43:31 -0500

    MOdoDeJuego agregado

---
+++ src/presentacion/ModoDeJuego.java
@@ -0,0 +1,98 @@
+package presentacion;
+
+import javax.swing.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.lang.Thread;
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class ModoDeJuego extends JDialog{
+    private JPanel botones;
+    private JButton unoVsUno;
+    private JButton maquinaVsMaquina;
+    private JButton unoVsMaquina;
+    private JButton volver;
+    private JPanel panelVolver;
+   
+    public ModoDeJuego(){
+        prepareElementos();
+        prepareAcciones();
+    }
+    public void prepareElementos(){
+        Dimension screen=Toolkit.getDefaultToolkit().getScreenSize();
+		setSize(screen.width/2,2*screen.height/3);
+        setLayout(new BorderLayout());
+        setBackground(Color.BLACK);
+        botones=new JPanel();
+		botones.setLayout(new  GridLayout(4,1,20,20));
+        botones.setBackground(Color.BLACK);
+        botones.setBorder(BorderFactory.createEmptyBorder(20,100,-130,-50));
+        elementosPanelBotones();
+        panelVolver=new JPanel();
+        panelVolver.setBackground(Color.BLACK);
+        panelVolver.setBorder(BorderFactory.createEmptyBorder(400,100,0,30));
+        elementosPanelVolver();
+        add(botones,BorderLayout.CENTER);
+        add(panelVolver,BorderLayout.EAST);
+    }
+    public void prepareAcciones(){
+        addWindowListener (
+			new WindowAdapter(){
+				public void windowClosing(WindowEvent e){
+					cerrarVentana();
+				}
+			}
+		);
+        volver.addActionListener(
+            new ActionListener(){
+                public void actionPerformed(ActionEvent e){
+                cerrar();    
+                }
+            }
+        );
+    }
+    public void cerrarVentana(){
+			if(JOptionPane.showConfirmDialog(null, "Estas seguro?")== JOptionPane.OK_OPTION){
+                setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			}else{
+				setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+			}
+	}
+    public void cerrar(){
+        if(JOptionPane.showConfirmDialog(null, "Estas seguro?")== JOptionPane.OK_OPTION){
+            dispose();
+        }
+    }
+    public void elementosPanelBotones(){
+        unoVsUno=new JButton();
+        unoVsUno.setBorderPainted(false);
+        maquinaVsMaquina=new JButton();
+        maquinaVsMaquina.setBorderPainted(false);
+        unoVsMaquina=new JButton(); 
+        unoVsMaquina.setBorderPainted(false);
+        unoVsUno.setBackground(Color.BLACK);
+        maquinaVsMaquina.setBackground(Color.BLACK);
+        unoVsMaquina.setBackground(Color.BLACK);
+        ImageIcon icono=new ImageIcon("imagenes/1vsM.png");
+        unoVsMaquina.setIcon(icono);
+        icono=new ImageIcon("imagenes/1vs1.png");
+        unoVsUno.setIcon(icono);
+        icono=new ImageIcon("imagenes/MvsM.png");
+        maquinaVsMaquina.setIcon(icono);
+        botones.add(unoVsUno);
+        botones.add(unoVsMaquina);
+        botones.add(maquinaVsMaquina);
+        
+    }
+    public void elementosPanelVolver(){
+        volver=new JButton();
+        ImageIcon icono=new  ImageIcon("imagenes/volver.png");
+        volver.setBorderPainted(false);
+        volver.setBackground(Color.BLACK);
+        volver.setIcon(icono);
+        panelVolver.add(volver);
+    }
+    
+}
